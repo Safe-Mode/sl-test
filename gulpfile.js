ef74bccd1730f2c3e9de267a95df273c0e33afb5
Safe_Mode <safe2mode@gmail.com>
2018-02-08 00:39:03 +0300

    Добавил утерянную картинку, убрал задачу вебп из галпфайла

--- gulpfile.js
+++ gulpfile.js
@@ -106,5 +106,5 @@ gulp.task("clean", function () {
 });
 
 gulp.task("build", function(done) {
-  run("clean", "copy", "style", "sprite", "script", "webp", "html", done);
+  run("clean", "copy", "style", "sprite", "script", "html", done);
 });
